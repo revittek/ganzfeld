e6aaba0de55a08f5c46e71ad9e6a0f6abdb40255
revittek <k.revitte@gmail.com>
2014-01-23 13:28:37 -0500

    added hertz selection dial removed press me button

--- platforms/ios/www/js/index-v3.js
+++ platforms/ios/www/js/index-v3.js
@@ -1,17 +1,9 @@
 var app = {
 	initialize: function() {
 	},
-    onReset: function() {
-		document.getElementById("title").style.visibility = "visible";
-    	document.getElementById("reset").style.display = "none";
-    	document.getElementById("knobby").style.visibility = "visible";
-    	document.body.style.backgroundColor = "white";
-    	clearInterval(myVar);
-    	$('dial').val(111).trigger('change');
-    	},
-    onInput: function(val) {
-    	console.log(val);
-    	document.getElementById("title").style.visibility = "hidden";
+	onClickButton: function() {
+		document.getElementById("title").style.visibility = "hidden";
+		document.getElementById("ready").style.display = "none";
     	document.getElementById("reset").style.display = "block";
     	document.getElementById("knobby").style.visibility = "hidden";
     	var state = false;
@@ -19,6 +11,28 @@ var app = {
             state = !state;
             var color = (state?'red':'white');
             document.body.style.backgroundColor = color;
-        }, 1000/(val/100));
+        }, 111);
+	},
+    onReset: function() {
+		document.getElementById("title").style.visibility = "visible";
+    	document.getElementById("ready").style.display = "block";
+    	document.getElementById("reset").style.display = "none";
+    	document.getElementById("knobby").style.visibility = "visible";
+    	document.body.style.backgroundColor = "white";
+    	clearInterval(myVar);
+    	document.getElementById("myDial").setAttribute("value","111");
+    	},
+    onInput: function(val) {
+    	console.log(val);
+    	document.getElementById("title").style.visibility = "hidden";
+		document.getElementById("ready").style.display = "none";
+    	document.getElementById("reset").style.display = "block";
+    	document.getElementById("knobby").style.visibility = "hidden";
+    	var state = false;
+    	myVar = setInterval(function(){
+            state = !state;
+            var color = (state?'red':'white');
+            document.body.style.backgroundColor = color;
+        }, val);
     }
 };
--- platforms/ios/www/js/index.js
+++ platforms/ios/www/js/index.js
@@ -1,25 +1,24 @@
 var app = {
 	initialize: function() {
-		document.getElementById("ready").style.display = "block";
-		document.getElementById("title").style.visibility = "hidden";
-		document.getElementById("reset").style.display = "none";
 	},
-	onClickButton: function() {
-		document.getElementById("title").style.visibility = "hidden";
-		document.getElementById("ready").style.display = "none";
+    onReset: function() {
+		document.getElementById("title").style.visibility = "visible";
+    	document.getElementById("reset").style.display = "none";
+    	document.getElementById("knobby").style.visibility = "visible";
+    	document.body.style.backgroundColor = "white";
+    	clearInterval(myVar);
+    	$('dial').val(111).trigger('change');
+    	},
+    onInput: function(val) {
+    	console.log(val);
+    	document.getElementById("title").style.visibility = "hidden";
     	document.getElementById("reset").style.display = "block";
+    	document.getElementById("knobby").style.visibility = "hidden";
     	var state = false;
     	myVar = setInterval(function(){
             state = !state;
             var color = (state?'red':'white');
             document.body.style.backgroundColor = color;
-        }, 111);
-	},
-    onReset: function() {
-		document.getElementById("title").style.visibility = "visible";
-    	document.getElementById("ready").style.display = "block";
-    	document.getElementById("reset").style.display = "none";
-    	document.body.style.backgroundColor = "white";
-    	clearInterval(myVar);
-    	}
+        }, 1000/(val/100));
+    }
 };
